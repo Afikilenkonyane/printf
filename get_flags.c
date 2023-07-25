a34f9433524c82197e43697bb35e320d6990e63e
pluto096 <oyewopelumi99@gmail.com>
2023-07-25 15:20:54 -0700

    updated files

--- get_flags.c
+++ get_flags.c
@@ -6,25 +6,25 @@
  * @j: take a parameter.
  * Return: Fla
  */
-int gmt_flags(const char *format, int *i)
+int gmt_flags(const char *format, int *j)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, cur_i;
+	int p, cur_i;
 	int flag = 0;
 	const char FLAG_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAG_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (cur_i = *i + 1; format[cur_i] != '\0'; cur_i++)
 	{
-		for (j = 0; FLAG_CH[j] != '\0'; j++)
-			if (format[cur_i] == FLAG_CH[j])
+		for (p = 0; FLAG_CH[p] != '\0'; p++)
+			if (format[cur_i] == FLAG_CH[p])
 			{
-				flag |= FLAG_ARR[j];
+				flag |= FLAG_ARR[p];
 				break;
 			}
 
-		if (FLAG_CH[j] == 0)
+		if (FLAG_CH[p] == 0)
 			break;
 	}
 
